692f108fd3b5f11d2822630bd05dc5e92d2cbc98
SUVRO1234 <102866798+SUVRO1234@users.noreply.github.com>
2022-04-03 20:53:03 +0530

    Add files via upload

---
+++ project-template-eoic-archery-5-main/js/Player.js
@@ -0,0 +1,45 @@
+class Player {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+
+    this.life4 = "green";
+    this.life5 = "green";
+    this.life6 = "green";
+
+    World.add(world, this.body);
+  }
+
+  life() {
+    push();
+   //write a correct code to show lives as three green rectangles in a strainght line.
+   fill(this.life4);
+   rect(width - 1275, 50, 70, 30);
+   fill(this.life5);
+   rect(width - 1345, 50, 70, 30);
+   fill(this.life6);
+   rect(width - 1205, 50, 70, 30);
+    pop();
+  }
+
+ 
+
+
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
